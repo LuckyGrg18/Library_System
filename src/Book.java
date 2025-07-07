bfe9f93faffcb8e8f53395ce2e333ebb86cdecd0
LuckyGrg18 <lucky109917@gmail.com>
2025-07-07 12:25:07 +0545

    first comit

---
+++ src/Book.java
@@ -0,0 +1,16 @@
+public class Book {
+    //Attributes -> book no., name, qty, author
+    //TODO create the attributes
+    int bookNumber;
+    String bookName;
+    int bookQuantity;
+    String bookAuthor;
+
+    //Behaviour -> increase qty, decrease qty
+    public void increaseQuantity() {
+        System.out.println("need work to increase");
+    }
+    public void decreaseQuantity() {
+        System.out.println("need work to decrease");
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,17 @@
+public class User {
+    //Attribute -> name, username, contact
+    String name;
+    String username;
+    int contact;
+
+    //Behaviour -> check is registered, borrow, return
+    public void isRegistered(){
+        System.out.println("need work to increase");
+    }
+    public void borrowBook(){
+        System.out.println("need work to increase");
+    }
+    public void returnBook(){
+        System.out.println("need work to increase");
+    }
+}
