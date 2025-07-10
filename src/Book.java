9f5fa44898402bcb0f4ec0359e97468b0ed8cfc3
LuckyGrg18 <lucky109917@gmail.com>
2025-07-10 13:01:37 +0545

    connecting database

--- src/Book.java
+++ src/Book.java
@@ -1,16 +1,62 @@
 public class Book {
-    //Attributes -> book no., name, qty, author
-    //TODO create the attributes
-    int bookNumber;
-    String bookName;
-    int bookQuantity;
-    String bookAuthor;
+    // attributes -> book number, name, quantity, author
+    // TODO create the attribute
+    private int bookNumber;
+    private String bookName;
+    private int bookQuantity;
+    private String bookAuthor;
 
-    //Behaviour -> increase qty, decrease qty
-    public void increaseQuantity() {
-        System.out.println("need work to increase");
+    public Book(int bookNumber, String bookName, int bookQuantity, String bookAuthor) {
+        this.bookNumber = bookNumber;
+        this.bookName = bookName;
+        this.bookQuantity = bookQuantity;
+        this.bookAuthor = bookAuthor;
+
     }
+
+    public int getBookNumber() {
+        return bookNumber;
+    }
+
+    public void setBookNumber(int bookNumber) {
+        this.bookNumber = bookNumber;
+    }
+
+    public int getBookQuantity() {
+        return bookQuantity;
+    }
+
+    public void setBookQuantity(int bookQuantity) {
+        this.bookQuantity = bookQuantity;
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public void setBookAuthor(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    // behaviour -> increase-quantity, decrease-quantity
     public void decreaseQuantity() {
-        System.out.println("need work to decrease");
+
+        this.bookQuantity--;
+        System.out.println("Need to work to increase");
     }
+
+    public void increaseQuantity() {
+
+        this.bookQuantity++;
+        System.out.println("Need to work to decrease");
+    }
+
 }
--- src/User.java
+++ src/User.java
@@ -1,17 +1,81 @@
 public class User {
-    //Attribute -> name, username, contact
-    String name;
-    String username;
-    int contact;
+    // attributes -> name, username, contact
+    private String name;
+    private String userName;
+    private int contact;
 
-    //Behaviour -> check is registered, borrow, return
-    public void isRegistered(){
-        System.out.println("need work to increase");
+
+    public User(String name, String userName, int contact) {
+        this.name = name;
+        this.userName = userName;
+        this.contact = contact;
     }
-    public void borrowBook(){
-        System.out.println("need work to increase");
+
+    // getters and setters
+    // getter of Name
+    public String getName() {
+        System.out.println(name);
+        return name;
     }
-    public void returnBook(){
-        System.out.println("need work to increase");
+
+    // setter of Name
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    // getter of UserName
+    public String getUserName() {
+        System.out.println(userName);
+        return userName;
+    }
+
+    //    setter of UserName
+    public void setUserName(String userName) {
+
+        this.userName = userName;
+    }
+
+    //    getter of Contact
+    public int getContact() {
+        System.out.println(contact);
+        return contact;
+    }
+
+    //    setter of Contact
+    public void setContact(int contact) {
+
+        this.contact = contact;
+    }
+
+    // behaviours -> check is registered?, borrow, return.
+
+    public void isRegistered() {
+
+        System.out.println("Need to work checking registered?");
+    }
+
+    // borrow books
+//    TODO Borrow a book
+    public void borrowBook(Book book, User user) {
+        System.out.println("need to work to Borrow book");
+
+        if (book.getBookQuantity() > 0) {
+            BookRecord record = new BookRecord(user, book);
+            book.decreaseQuantity();
+
+            System.out.println("Book borrowed successfully!!");
+
+        } else {
+            System.out.println("Book not available");
+        }
+
+    }
+
+    // return books
+    // TODO return books
+    public void returnBook() {
+
+        System.out.println("need to work to return book");
     }
 }
